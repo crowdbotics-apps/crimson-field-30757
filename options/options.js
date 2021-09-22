20c0176b69955174c20de909f3bf0f8d63bdea8b
Crowdbotics <team@crowdbotics.com>
2021-09-22 21:36:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "crimson_field_30757Identifier",
+  url: "https://crimson_field_30757Identifier.botics.co",
+  api: "https://crimson_field_30757Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
